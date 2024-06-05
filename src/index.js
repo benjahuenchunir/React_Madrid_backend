57c5032354b7c8e72b1808ad3d0422ff339a5e06
benjahuenchunir <bahuenchunir@gmail.com>
2024-06-05 15:21:14 -0400

    feat(base_project): add setup files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-const Koa = require('koa');
-const Logger = require('koa-logger');
+const app = require('./app');
+const db = require('../models');
+const dotenv = require('dotenv');
 
-const app = new Koa();
+dotenv.config();
 
-app.use(Logger());
+const PORT = process.env.PORT || 3000;
 
-app.use((ctx, next) => {
-  ctx.body = 'Hola mundo';
-});
-
-app.listen(3000);
+db.sequelize
+    .authenticate()
+    .then(() => {
+        console.log('La conexión a la base de datos ha sido establecida de manera exitosa');
+        app.listen(PORT, (error) => {
+            if (error) {
+                return console.error("Error: ", error);
+            }
+            console.log(`Escuchando en el puerto ${PORT}`);
+            return app;
+        })
+    })
+    .catch((error) => { console.log('No ha sido posible conectarse a la base de datos: ', error); });
