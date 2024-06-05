5e11e639a527ec6be751c9b5139e782d00df61ee
benjahuenchunir <bahuenchunir@gmail.com>
2024-06-05 19:32:10 -0400

    feat(seeds): create users, chats and members

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 * :link: **Backend:** https://react-madrid-backend.onrender.com/
 
+## Setup
+
+1. Crear archivo `.env` copiando el archivo `.env.example`
+2. Rellenar el archivo `.env` con las credenciales correspondientes
+3. Ejecutar postgresql:
+
+```bash
+sudo service postgresql start
+```
+
+4. Crear la base de datos y poblarla:
+
+```bash
+yarn db:reset
+```
+
 ## Diagrama Entidad-Relación :scroll:
 ![ER-Model](assets/ER-Model.png)
 
---
+++ src/seeders/20240605232941-create_members.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface) {
+    return queryInterface.bulkInsert('Members', [
+      {
+        id_chat: 1,
+        id_user: 1,
+        role: 'owner',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id_chat: 1,
+        id_user: 2,
+        role: 'member',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id_chat: 2,
+        id_user: 3,
+        role: 'owner',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id_chat: 2,
+        id_user: 2,
+        role: 'member',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id_chat: 2,
+        id_user: 1,
+        role: 'member',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  async down (queryInterface) {
+    return queryInterface.bulkDelete('Members', null, {});
+  }
+};
