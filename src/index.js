c61dfd95ae6fe6f30b6d8874a71bfefaeb633e49
benjahuenchunir <bahuenchunir@gmail.com>
2024-06-05 14:31:07 -0400

    feat(base_project): add base project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const Koa = require('koa');
+const Logger = require('koa-logger');
+
+const app = new Koa();
+
+app.use(Logger());
+
+app.use((ctx, next) => {
+  ctx.body = 'Hola mundo';
+});
+
+app.listen(3000);
