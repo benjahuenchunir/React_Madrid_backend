427281b1d2e185a53b5ee0d01d5a6e8d8c3dc0a2
benjahuenchunir <bahuenchunir@gmail.com>
2024-06-08 18:30:18 -0400

    feat(chats): send user chats or dms

--- src/routes/chats.js
+++ src/routes/chats.js
@@ -1,18 +1,41 @@
 const Router = require('koa-router');
-const { Chat, Message } = require('../models');
+const { Chat, User } = require('../models');
 const router = new Router();
 
 router.get('/', async (ctx) => {
     try {
-        const chats = await Chat.findAll({
-            include: [
-                {
-                    model: Message
-                }
-            ]
+        const userId = Number(ctx.query.userId);
+
+        const allChats = await Chat.findAll({
+            attributes: ['name', 'image_url'],
+            include: [{
+                model: User
+            }]
         });
+        
+        const userChats = allChats.filter(chat => 
+            chat.Users.some(user => user.id === userId)
+        );
+
+        const modifiedChats = userChats.map(chat => {
+            let modifiedChat = { ...chat.toJSON() };
+
+            if (modifiedChat.Users.length === 2) {
+                const otherUser = modifiedChat.Users.find(user => user.id !== userId);
+
+                modifiedChat.name = `${otherUser.name} ${otherUser.last_name}`;
+                modifiedChat.image_url = otherUser.profile_picture_url;
+            }
+
+            return {
+                id: modifiedChat.id,
+                name: modifiedChat.name,
+                image_url: modifiedChat.image_url
+            };
+        });
+
         ctx.status = 200;
-        ctx.body = chats;
+        ctx.body = modifiedChats;
     } catch (error) {
         ctx.status = 500;
         ctx.body = { error: error.message };
