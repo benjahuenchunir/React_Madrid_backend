8695fe5c4ae1bd7641f4e5583f5010557e5564b1
benjahuenchunir <bahuenchunir@gmail.com>
2024-06-09 11:36:22 -0400

    feat(chats): send chat messages

--- src/routes/chats.js
+++ src/routes/chats.js
@@ -2,6 +2,19 @@ const Router = require('koa-router');
 const { Chat, User, Message} = require('../models');
 const router = new Router();
 
+function transformChat(chat, userId) {
+    let modifiedChat = { ...chat.toJSON() };
+
+    if (modifiedChat.Users.length === 2) {
+        const otherUser = modifiedChat.Users.find(user => user.id !== userId);
+
+        modifiedChat.name = `${otherUser.name} ${otherUser.last_name}`;
+        modifiedChat.image_url = otherUser.profile_picture_url;
+    }
+
+    return modifiedChat;
+}
+
 router.get('/', async (ctx) => {
     try {
         const userId = Number(ctx.query.userId);
@@ -22,15 +35,7 @@ router.get('/', async (ctx) => {
         );
 
         const modifiedChats = userChats.map(chat => {
-            let modifiedChat = { ...chat.toJSON() };
-
-            if (modifiedChat.Users.length === 2) {
-                const otherUser = modifiedChat.Users.find(user => user.id !== userId);
-
-                modifiedChat.name = `${otherUser.name} ${otherUser.last_name}`;
-                modifiedChat.image_url = otherUser.profile_picture_url;
-            }
-
+            const modifiedChat = transformChat(chat, userId);
             return {
                 id: modifiedChat.id,
                 name: modifiedChat.name,
@@ -44,6 +49,48 @@ router.get('/', async (ctx) => {
 
         ctx.status = 200;
         ctx.body = modifiedChats;
+    } catch (error) {
+        ctx.status = 500;
+        ctx.body = { error: error.message };
+    }
+});
+
+router.get('/:id', async (ctx) => {
+    try {
+        const chatId = Number(ctx.params.id);
+
+        const chat = await Chat.findOne({
+            where: { id: chatId },
+            include: [{
+                model: User
+            }, {
+                model: Message,
+                order: [['createdAt', 'DESC']],
+                include: [{
+                    model: User,
+                    attributes: ['id', 'name']
+                }]
+            }]
+        });
+
+        if (!chat) {
+            ctx.status = 404;
+            ctx.body = { error: 'No se encontró el chat' };
+            return;
+        }
+
+        const messages = chat.Messages.map(message => ({
+            id: message.id,
+            message: message.message,
+            time: message.createdAt,
+            user: {
+                id: message.User.id,
+                name: message.User.name
+            }
+        }));
+
+        ctx.status = 200;
+        ctx.body = messages;
     } catch (error) {
         ctx.status = 500;
         ctx.body = { error: error.message };
