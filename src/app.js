856767d1a99d3faf2d7e9d5eb44687bac24e70a6
benjahuenchunir <bahuenchunir@gmail.com>
2024-06-14 12:37:22 -0400

    docs: add basic structure

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const Koa = require('koa');
 const Logger = require('koa-logger');
 const { koaBody } = require('koa-body');
+const fs = require('fs');
+const yamljs = require('yamljs');
+const {koaSwagger} = require('koa2-swagger-ui');
 const router = require('./routes.js');
 const orm = require('./models/index.js');
 const cors = require('@koa/cors');
@@ -15,6 +18,24 @@ app.use(cors());
 // Middlewares proporcionados por Koa
 app.use(Logger());
 app.use(koaBody());
+
+// .load loads file from root.
+let spec = fs.readFileSync('./api.yaml', 'utf8');
+spec = spec.replace('${DB_HOST}', process.env.DB_HOST);
+spec = spec.replace('${PORT}', process.env.PORT);
+spec = yamljs.parse(spec);
+
+app.use(koaSwagger({
+    routePrefix: '/swagger',
+    swaggerOptions: { spec },
+  })
+)
+
+app.use(koaSwagger({ // TODO delete after testing
+    routePrefix: '/swagger-example',
+    swaggerOptions: { url: 'http://petstore.swagger.io/v2/swagger.json'  },
+  })
+)
 
 // koa router
 app.use(router.routes());
