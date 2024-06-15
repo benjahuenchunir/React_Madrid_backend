38cddb22f84245b530217f8eb5334ce9037c38fa
fvidalf <fvidalf@uc.cl>
2024-06-14 21:47:09 -0400

    feat(patch-user): Add patch route for user

--- src/routes/users.js
+++ src/routes/users.js
@@ -40,6 +40,61 @@ router.post('/', async (ctx) => {
     }
 });
 
+router.patch('/edit', upload.single('file'), async (ctx) => {
+    try {
+        const { name, last_name, email, password, phone } = ctx.request.body;
+        const file = ctx.request.file;
+
+        let user = await User.findOne({ where: { email } });
+        if (!user) {
+            ctx.status = 404;
+            ctx.body = { message: 'User not found' };
+            return;
+        }
+
+        let profilePictureUrl = null;
+        if (file) {
+            try {
+                const result = await cloudinary.uploader.upload(file.path);
+                profilePictureUrl = result.url;
+                await unlink(file.path);
+            } catch (uploadError) {
+                console.log(uploadError);
+                ctx.status = 500;
+                ctx.body = { message: 'Error uploading file to Cloudinary, the file was ' + file.path + ' ' + uploadError.message
+                };
+                return;
+            }
+        }
+
+        try {
+            const saltRounds = 5;
+            const hashPassword = await bcrypt.hash(password, saltRounds);
+            user = await User.update({
+                name,
+                last_name,
+                email,
+                password: hashPassword,
+                phone,
+                profile_picture_url: profilePictureUrl,
+                role: 'user'
+            }, {where: { email: email }});
+        } catch (error) {
+            ctx.status = 500;
+            ctx.body = { message: "Error updating user " + error.message};
+            return;
+        }
+
+        ctx.status = 201;
+        ctx.body = { message: `User of name ${user.name} and email ${user.email} updated` };
+
+    } catch (error) {
+        console.log(error);
+        ctx.status = 500;
+        ctx.body = { message: error.message };
+    }
+});
+
 router.delete('/:id', async (ctx) => {
     try {
         const user = await User.findByPk(ctx.params.id);
