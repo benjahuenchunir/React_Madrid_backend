0a076307df7426c0c43d0eadec23dbc3e10d64fe
benjahuenchunir <bahuenchunir@gmail.com>
2024-06-21 21:13:21 -0400

    feat(websockets): create websocketManager

---
+++ src/websocketManager.js
@@ -0,0 +1,39 @@
+class WebSocketManager {
+  constructor() {
+    this.connections = new Map();
+  }
+
+  addConnection(idChat, ws) {
+    if (!this.connections.has(idChat)) {
+      this.connections.set(idChat, new Set());
+    }
+    this.connections.get(idChat).add(ws);
+    ws.on('close', () => this.removeConnection(idChat, ws));
+  }
+
+  removeConnection(idChat, ws) {
+    if (this.connections.has(idChat)) {
+      const chatConnections = this.connections.get(idChat);
+      chatConnections.delete(ws);
+      if (chatConnections.size === 0) {
+        this.connections.delete(idChat);
+      }
+    }
+  }
+
+  broadcast(message) {
+    console.log(message)
+    if (this.connections.has(message.idChat)) {
+      const chatConnections = this.connections.get(message.idChat);
+      for (let ws of chatConnections) {
+        if (ws.readyState === ws.OPEN && ws.idUser !== message.user.id) {
+          ws.send(JSON.stringify(message));
+        }
+      }
+    } else {
+        console.log("Not Sending")
+    }
+  }
+}
+
+module.exports = new WebSocketManager();
