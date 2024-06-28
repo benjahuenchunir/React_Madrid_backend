6506ea4e2356d645734dcf334bfe695ce1a48668
benjahuenchunir <bahuenchunir@gmail.com>
2024-06-28 14:10:28 -0400

    feat(chats): create dm

--- src/routes/chats.js
+++ src/routes/chats.js
@@ -1,24 +1,9 @@
 const Router = require('koa-router');
-const { Chat, User, Message, MessageFile } = require('../models');
+const { Chat, User, Message, MessageFile, Member } = require('../models');
 const router = new Router();
-const { canSendMessage } = require('../utils/permissions');
-
-function transformChat(chat, userId) {
-    let modifiedChat = { ...chat.toJSON() };
-
-    if (isDM(chat)) {
-        const otherUser = modifiedChat.Users.find(user => user.id !== userId);
-
-        modifiedChat.name = `${otherUser.name} ${otherUser.last_name}`;
-        modifiedChat.image_url = otherUser.profile_picture_url;
-    }
-
-    return modifiedChat;
-}
-
-function isDM(chat) {
-    return chat.Users.length === 2;
-}
+const { getUserIdFromToken } = require('../utils/auth');
+const { literal } = require('sequelize')
+const db = require('../models/index.js')
 
 router.get('/', async (ctx) => {
     try {
@@ -26,36 +11,23 @@ router.get('/', async (ctx) => {
 
         const allChats = await Chat.findAll({
             attributes: ['name', 'image_url', 'mode'],
-            include: [{
-                model: User
-            }, {
-                model: Message,
-                order: [['id', 'DESC']],
-                limit: 1,
-            }]
+            include: [
+                {
+                    model: User
+                },
+                {
+                    model: Message,
+                    order: [['id', 'DESC']],
+                    limit: 1,
+                }
+            ]
         });
 
         const userChats = allChats.filter(chat =>
             chat.Users.some(user => user.id === userId)
         );
 
-        const modifiedChats = await Promise.all(userChats.map(async chat => {
-            const modifiedChat = transformChat(chat, userId);
-            const lastMessage = modifiedChat.Messages[0];
-            return {
-                id: modifiedChat.id,
-                name: modifiedChat.name,
-                imageUrl: modifiedChat.image_url,
-                canSendMessage: await canSendMessage(userId, modifiedChat.id),
-                lastMessage: lastMessage
-                    ? {
-                        message: lastMessage.message,
-                        time: lastMessage.createdAt
-                      }
-                    : null,
-                isDm: isDM(chat)
-            };
-        }));
+        const modifiedChats = await Promise.all(userChats.map(async chat => await chat.toDomain(userId)))
 
         ctx.status = 200;
         ctx.body = modifiedChats;
@@ -92,7 +64,6 @@ router.get('/:id', async (ctx) => {
             return;
         }
 
-
         const messages = await Promise.all(chat.Messages.map(message => message.getFullMessage()));
 
         ctx.status = 200;
@@ -105,14 +76,70 @@ router.get('/:id', async (ctx) => {
 });
 
 router.post('/', async (ctx) => {
+    const transaction = await db.sequelize.transaction();
     try {
-        const newChat = await Chat.create(ctx.request.body);
+        const idUser = getUserIdFromToken(ctx);
+
+        const { name, image_url, mode, users } = ctx.request.body;
+        const newChat = await Chat.create({ name, image_url, mode }, { transaction });
+
+        for (const user of users) {
+            await Member.create({
+                id_chat: newChat.id,
+                id_user: user.id,
+                role: user.role
+            }, { transaction });
+        }
+
+        await transaction.commit();
         ctx.status = 201;
-        ctx.body = newChat;
+        ctx.body = await newChat.toDomain(idUser);
     } catch (error) {
+        console.error(error)
+        await transaction.rollback();
         ctx.status = 500;
         ctx.body = { error: error.message };
     }
 });
+
+router.get('/users/:id', async (ctx) => {
+    try {
+        const idUser = getUserIdFromToken(ctx);
+        const idOtherUser = Number(ctx.params.id);
+
+        const chat = await Chat.findOne({
+            include: [{
+                model: User,
+                attributes: [], // Exclude User attributes from the final result
+                through: {
+                    attributes: [] // Exclude attributes from Member
+                }
+            }],
+            where: literal(`
+                EXISTS (
+            SELECT 1
+            FROM "Members" AS "Member"
+            WHERE "Member"."id_chat" = "Chat"."id"
+            GROUP BY "Member"."id_chat"
+            HAVING COUNT(DISTINCT "Member"."id_user") = 2
+            AND ARRAY_AGG(DISTINCT "Member"."id_user") @> ARRAY[${idUser}, ${idOtherUser}]
+            AND ARRAY[${idUser}, ${idOtherUser}] @> ARRAY_AGG(DISTINCT "Member"."id_user")
+        )
+            `)
+        });
+
+        if (!chat) {
+            ctx.status = 404;
+            ctx.body = { error: 'Chat not found' };
+            return;
+        }
+
+        ctx.status = 200;
+        ctx.body = chat.id;
+    } catch (error) {
+        ctx.status = 500;
+        ctx.body = { error: error.message };
+    }
+})
 
 module.exports = router;
--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -1,39 +1,11 @@
-const { Chat, Member } = require('../models');
-const jwt = require('jsonwebtoken');
 
-async function canSendMessage(idUser, idChat) {
-    const chat = await Chat.findOne({ where: { id: idChat } });
-    const member = await Member.findOne({ where: { id_chat: idChat, id_user: idUser } });
+async function canSendMessage(chat, idUser) {
+    const members = await chat.getMembers()
+    const member = members.find(member => member.id_user === idUser);
     if (!member || (chat.mode !== 'everyone' && member.role === 'member')) {
-        return false;
+      return false;
     }
     return true;
 }
 
-function getTokenFromHeader(ctx) {
-    const authHeader = ctx.request.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new Error('No token provided');
-    }
-    return authHeader.split(' ')[1];
-}
-
-function getTokenFromParam(ctx) {
-    const token = ctx.query.token;
-    if (!token) {
-        throw new Error('No token provided');
-    }
-    return token;
-}
-
-function getUserIdFromToken(ctx, tokenGetter = getTokenFromHeader) {
-    const token = tokenGetter(ctx);
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        return Number(decoded.sub);
-    } catch (error) {
-        throw new Error('Invalid token');
-    }
-}
-
-module.exports = { canSendMessage, getUserIdFromToken, getTokenFromParam };
+module.exports = { canSendMessage };
